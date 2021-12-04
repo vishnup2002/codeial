31cfbeb6e09bd6195fa0f47d4f4c87c4e0f8a04b
Vishnu <vishnupothia@gmail.com>
2021-12-04 15:56:46 +0530

    add multiple routers and controllers

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile = function(req,res){
+    res.end('<h1>Profile</h1>');
+}
+
+module.exports.user_home = function(req,res){
+    res.end('<h1>User1</h1><h2>user 2</h2>');
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const homeController = require('../controllers/homeController');
 
-router.get('/',homeController.home);
 
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 module.exports = router;
